b593488915c59d0bbd59dcd1bf56562ec4c2e2d2
Arthur <zyujiawei@hotmail.com>
2016-03-31 16:35:03 +0800

    nav bar

--- public/js/main.js
+++ public/js/main.js
@@ -25,15 +25,15 @@ function main() {
     Preloader
     ======================================*/
 	
-  	$(window).load(function() {
+  	// $(window).load(function() {
 
-   	// will first fade out the loading animation 
-    	$("#status").fadeOut("slow"); 
+   // 	// will first fade out the loading animation 
+   //  	$("#status").fadeOut("slow"); 
 
-    	// will fade out the whole DIV that covers the website. 
-    	$("#preloader").delay(500).fadeOut("slow").remove();      
+   //  	// will fade out the whole DIV that covers the website. 
+   //  	$("#preloader").delay(500).fadeOut("slow").remove();      
 
-  	}) 
+  	// }) 
     /*====================================
     Show Menu on Book
     ======================================*/
